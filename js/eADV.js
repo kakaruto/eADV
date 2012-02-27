dd9c652ee82705d70016b83ab13735290fb13a0c
Philippe GARRY <philippe.garry@valtech.fr>
2012-02-27 15:59:09 +0100

    Menu popup

--- js/eADV.js
+++ js/eADV.js
@@ -49,6 +49,105 @@ var css = function ( el, props ) {
     return el;
 };
 
+var initMenu = function() {
+	var menu = byId('etudes_menu');
+
+    drawMenuBackground(menu.offsetWidth, menu.offsetHeight);
+
+    menu.addEventListener('webkitTransitionEnd', function(e){
+        if(menu.getAttribute('class') == 'hide'){
+            menu.style.setProperty('display','none');
+        }
+    });
+
+    menu.setAttribute('class', 'hide');
+};
+
+var drawMenuBackground = function (rectWidth, rectHeight){
+
+    var context = document.getCSSCanvasContext('2d', 'menu_background', rectWidth, rectHeight);    
+
+    var arrowHeight = 20;
+    var radius = 6;
+    var lineWidth = 1;
+    var pad = lineWidth/2;
+    var xs = pad;
+    var ys = pad + arrowHeight;
+    var xe = rectWidth - pad;
+    var ye = rectHeight - pad;
+
+    var gradient = context.createLinearGradient(rectWidth/2, 0, rectWidth/2, arrowHeight * 2);
+    gradient.addColorStop(0, '#eee'); 
+    gradient.addColorStop(1, '#151d31'); 
+
+    context.beginPath();
+
+    context.lineJoin = 'miter';
+
+    context.moveTo(xs + radius, ys);
+
+    //console.log(rectWidth);
+
+    // context.lineTo(rectWidth/2 - (arrowHeight + pad), ys);
+    // context.lineTo(rectWidth/2, pad);
+    // context.lineTo(rectWidth/2 + (arrowHeight + pad), ys);
+
+    context.lineTo(xe - radius, ys);
+
+    context.arcTo(xe, ys, xe, ys + radius, radius);
+
+    context.lineTo(xe, ye - radius);
+    context.arcTo(xe, ye, xe - radius, ye, radius);
+
+    context.lineTo(xs + radius, ye);
+    context.arcTo(xs, ye, xs, ye - radius, radius);
+
+    context.lineTo(xs, ys + radius);
+    context.arcTo(xs, ys, xs + radius, ys, radius);
+
+    context.fillStyle = gradient;
+
+    //context.fillStyle = '#000';
+    context.globalAlpha = .95;
+    context.fill();
+
+    context.globalAlpha=1;
+
+    context.strokeStyle = '#48484a';
+    context.lineWidth = lineWidth;
+    context.stroke();
+};
+
+
+var showMenu = function (el){
+
+    var menu = byId('etudes_menu');
+
+    menu.style.setProperty('display','block');    
+
+    var targetLeft = el.offsetLeft;
+    var targetBottom = el.offsetHeight;
+    var targetWidth = el.offsetWidth;    
+    var menuWidth = menu.offsetWidth;
+
+    var menuLeft = targetLeft + (targetWidth/2) - (menuWidth/2);
+
+    menu.style.setProperty('top', (768 - (menu.offsetHeight + 40)) + 'px');
+    menu.style.setProperty('left', menuLeft + 'px');
+
+    menu.setAttribute('class', 'show');
+
+    // menu.onclick = function(e){
+    //     if(e.target.tagName.toLowerCase() == 'a'){
+    //         var type = e.target.innerHTML;
+    //         var link = el.getAttribute('href');
+    //         alert(type + 'ing ' + link);
+    //         menu.setAttribute('class','hide');
+    //     }
+    // }
+};
+
+
 (function (document,window) {
     'use strict';
 
@@ -171,6 +270,44 @@ var css = function ( el, props ) {
         }
     }, false);
 
+    document.addEventListener("click", function ( event ) {
+        // event delegation with "bubbling"
+        // check if event target (or any of its parents is a link or a step)
+        var target = event.target;
+        while ( (target.tagName != "A") &&
+                (!target.stepData) &&
+                (target != document.body) ) {
+            target = target.parentNode;
+        }
+        
+        if ( target.tagName == "A" ) {
+            var href = target.getAttribute("href");
+            
+            // if it's a link to presentation step, target this step
+            if ( href && href[0] == '#' ) {
+                target = byId( href.slice(1) );
+            }
+        }
+        else
+        {
+        	var menu = byId('etudes_menu');
+	        if (menu.classList.contains('show'))
+	        {
+	        	menu.setAttribute('class','hide');
+	        }
+        }
+        
+        if ( select(target) ) {
+            event.preventDefault();
+        }
+
+
+
+        
+        
+    }, false);
+
+	initMenu();
     // Start with first step
     select(steps[0]);
 
